c9f706d78354634093ef7f3075a306e591b95f35
crazypushok0 <crazypushok0@gmail.com>
2020-05-09 02:14:50 +0300

    pochti konets slider

--- app/js/main.js
+++ app/js/main.js
@@ -12,8 +12,24 @@ $(document).ready(function() {
 
 
 	$('.slider__inner').slick({
-		arrows:false,
+		arrows:true,
+		dots:true,
+		appendDots:$('.slider-dots'),
+		appendArrows:$('.slider-arrows'),
+		speed:600,
+		autoplay:true,
+		autoplaySpeed: 3000,
+		prevArrow:'<button class="slick-prev"><img src="img/slider-banner/svg/fragments/left.svg" alt="" /></button>',
+		nextArrow:'<button class="slick-next"><img src="img/slider-banner/svg/fragments/right.svg" alt="" /></button>',
 	});
+	$('.slider').hover(function(){
+		$(this).children('.slider-arrows').stop(false , true).fadeIn(300);
+
+
+	},function(){
+		$(this).children('.slider-arrows').stop(false , true).fadeOut(300);
+	});
+
 });
 
 
